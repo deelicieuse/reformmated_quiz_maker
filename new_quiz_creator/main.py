60eb7aec962b7ddbfdd172403652cf3a7f855261
deelicieuse <libatiqueangelique@gmail.com>
2025-05-28 21:09:00 +0800

    added (if __name__ == "__main__":) from original file

--- new_quiz_creator/main.py
+++ new_quiz_creator/main.py
@@ -1,3 +1,8 @@
 import tkinter as tk
 from class_quiz_creator import QuizCreatorApp
 
+if __name__ == "__main__":
+    main_window = tk.Tk()
+    main_window.config(padx=15, pady=15)
+    quiz_app = QuizCreatorApp(main_window)
+    main_window.mainloop()
